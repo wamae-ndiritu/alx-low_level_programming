36fc2999bdbc1bae431adf56ede5827d4a4ec11e
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-03-16 03:19:12 -0700

    updated my betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * Description: prints "Programming is like building a multilingual puzzle
  * Return: returns 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
