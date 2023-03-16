75ad5815b8ebc745570232575cb5dfe556c85691
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-03-16 03:03:05 -0700

    improved my betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
-int main (void)
+/**
+ * using puts to print a statement on the screen
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
