80b6e9eec8867a55b3ba178863ecaec8b5a8940e
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-03-16 04:58:40 -0700

    formatted my printf program with bettycode style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,11 @@
  * @void - Takes no arguments
  *
  * Description - prints 'with proper grammar, but the outcome is a piece of art,' to the screen
- * Return - return 0
+ * Return: returns 0 when successful
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar,
+		       	but the outcome is a piece of art, ");
 	return (0);
 }
