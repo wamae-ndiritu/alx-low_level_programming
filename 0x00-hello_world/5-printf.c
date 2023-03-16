b1df99d6216cb8f3d23c020e9623828f99c76c5b
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-03-16 11:38:50 -0700

    checked betty styles

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
  * main - main entry point of the program
  * @void - Takes no arguments
  *
- * Description - prints 'with proper grammar, but the outcome is a piece of art,' to the screen
+ * Description - prints 'with proper grammar,
+ * but the outcome is a piece of art,' to the screen
  * Return: returns 0 when successful
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is "
-			"a piece of art, ");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
