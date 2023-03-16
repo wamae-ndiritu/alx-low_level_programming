c7bffd60c65c89c3de3694bd5c200ef0e102bb03
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-03-16 04:49:24 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main entry point of the program
+ * @void - Takes no arguments
+ *
+ * Description - prints 'with proper grammar, but the outcome is a piece of art,' to the screen
+ * Return - return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
