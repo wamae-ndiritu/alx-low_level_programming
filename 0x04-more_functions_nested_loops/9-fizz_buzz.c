d1e68230f87d543323265fef63b48ba34f5478b1
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-03-23 15:17:48 -0700

    modified FizzBuzz program

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,13 +18,15 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (num % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (num % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("%d ", num);
+			printf("%d", num);
+		if (num != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
