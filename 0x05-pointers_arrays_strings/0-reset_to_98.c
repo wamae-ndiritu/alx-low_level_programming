7a41996482a9c077ea9bd9234c2b762aedf1206c
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-03-27 20:20:35 +0000

    updating the value pointed to by a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer as the parameter and updates
+ * the value it points to to 98
+ *
+ * @n: pointer passed whose value it points to
+ * will be updated
+ *
+ * Description - updates the value the pointer points to
+ *
+ * Return: returns no value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,3 @@
 # Pointers, Arrays & Strings
+
+[0. 98 Battery st.](./0-reset_to_98.c) - Creating a function that takes a pointer to an int as parameter and updates the value it points to to 98. This is called `dereferncing` where we can manipulate the value of that the pointer points to.
