86a9ec4c035b71df2b1bfdd232d7347dfba04470
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-03-31 09:32:33 +0000

    modified the magic file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * leet - encodes a string into 1337
+ *
+ * @str: pointer to the string being encoded
+ *
+ * Return: returns a pointer to the encoded string
+ */
+
+char *leet(char *str)
+{
+
