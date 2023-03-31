c2ca5f220084898bfb969c37d44c7b438a2f594e
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-03-31 09:36:43 +0000

    improved betty coding style on magic file

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,9 +6,9 @@ int main(void)
 	int a[5];
 	int *p;
 
-       	a[2] = 1024;
+	a[2] = 1024;
 	p = &n;
-       	/*
+	/*
 	 * write your line of code here...
 	 * Remember:
 	 * you are not allowed to use a
