8ea1af9302600e68eb917d8481c0bb07b4fc0920
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-01 11:47:48 +0000

    modified print_numbers function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,14 +12,20 @@
 
 void print_number(int n)
 {
+	unsigned int num;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		num = -n;
 	}
-	if (n >= 10)
+	else
 	{
-		print_number(n / 10);
+		num = n;
 	}
-	_putchar(n % 10 + '0');
+	if (num >= 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar(num % 10 + '0');
 }
