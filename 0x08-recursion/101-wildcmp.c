1a7943572a94bdc9f7125c8aca462a8561f9b476
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-06 22:54:39 +0000

    comparing two functions using recursion && no looping

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,15 +18,11 @@ int wildcmp(char *s1, char *s2)
 		{
 			return (1);
 		}
-		while (*s1 != '\0')
+		if (*s1 == '\0')
 		{
-			if (wildcmp(s1, s2 + 1))
-			{
-				return (1);
-			}
-			s1++;
+			return (0);
 		}
-		return (0);
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 	else
 	{
