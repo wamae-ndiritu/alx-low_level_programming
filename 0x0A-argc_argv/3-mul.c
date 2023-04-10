4480aa29e941ee7d4c6b6f03d293ba1fbef79d43
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-10 08:51:30 +0000

    multplying 2 numbers passed as command line arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of the program
+ *
+ * @argc: number of command line arguments passed
+ *	to the main function
+ * @argv: an array of command line arguments passed
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int product_res, num1, num2;
+
+	if (argc >= 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		product_res = num1 * num2;
+		printf("%d\n", product_res);
+		return (0);
+	}
+	printf("Error");
+	return (1);
+}
