5a2d03683034814f9bbc44af6f1f6a6cc49b21a1
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-10 10:51:42 +0000

    adding numbers passed as command line arguments using argc and argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of the program
+ *
+ * @argc: number of command line arguments passed to the
+ *	main function
+ * @argv: an array of command line arguments passed to
+ *	the main function
+ * Return: returns 0, if success, 1 otherwise.
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int num, i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			num = atoi(argv[i]);
+			if (num == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += num;
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	printf("%d\n", 0);
+	return (1);
+}
