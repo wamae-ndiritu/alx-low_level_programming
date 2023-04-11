9014d41bb712be6f1a7eb2e8b05d1d5116d73130
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-11 15:31:19 +0000

    allocated memory, copied a string to it, and then concatenated another string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatenates two strings
+ *
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ *
+ * Return: returns pointer to newly allocated
+ *	space in memory containing s1 and s2.
+ *
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, str_len;
+	char *concatenated_str;
+
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+	else if (s2 == NULL)
+	{
+		s2 = " ";
+	}
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	str_len = len1 + len2;
+	concatenated_str = (char *)malloc(sizeof(char) * (str_len + 1));
+	if (concatenated_str == NULL)
+	{
+		return (NULL);
+	}
+	/* duplicating s1 to the new allocated space */
+	strcpy(concatenated_str, s1);
+	/* adding s2 to the s1 above */
+	strcat(concatenated_str, s2);
+	return (concatenated_str);
+
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -5,4 +5,7 @@
 
 [1. The woman who has no imagination has no wings](./1-strdup.c) - Creating a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
 
-`
+
+[2. He who is not courageous enough to take risks will accomplish nothing in life](./str_concat.c) - Creating a function taht concatenates two strings. The returned pointer should point to a newly allocated space in memory which conatins the contents of s1, followed by the contents of s2, and null terminated. If NULL is passed, we are treating it as an empty string.
+
+
