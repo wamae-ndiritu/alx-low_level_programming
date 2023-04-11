6ef1e8931daca3bdbb05f4094988e00ea7a76703
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-11 15:34:31 +0000

    fixed the case when strcat() takes NULL as the parameter

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,11 +21,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 	else if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
 	len1 = strlen(s1);
 	len2 = strlen(s2);
