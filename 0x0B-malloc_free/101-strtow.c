35e07419c5df177197d28ab7095046f9d6e03dae
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-12 21:19:53 +0000

    used a helper function to avoid too many codes in the function

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,6 +3,56 @@
 #include <string.h>
 
 /**
+ * split_str - handles the string spliting
+ * @s: the string to be splitted
+ * @str_array: memory block allocated to
+ *	store array of strings
+ * Return: returns an array of string
+ */
+
+char **split_str(char *s, char **str_array)
+{
+	int i = 0, j, word_len;
+
+	while (*s != '\0')
+	{
+		/* skip leading whitespace */
+		while (*s == ' ')
+		{
+			s++;
+		}
+		if (*s != '\0')
+		{
+			char *word_start;
+
+			word_start = s;
+			/* find the end of the word */
+			while (*s != ' ' && *s != '\0')
+			{
+				s++;
+			}
+			/* Allocate memory for the word */
+			word_len = s - word_start;
+			str_array[i] = (char *)malloc((word_len + 1) * sizeof(char));
+			if (str_array[i] == NULL)
+			{
+				/* free memory for previously allocated words */
+				for (j = 0; j < i; j++)
+				{
+					free(str_array[j]);
+				}
+				free(str_array);
+				return (NULL);
+			}
+			/* copy the word into the array */
+			strncpy(str_array[i], word_start, word_len);
+			str_array[i][word_len] = '\0';
+			i++;
+		}
+	}
+	return (str_array);
+}
+/**
  * strtow - splits a string into words
  * @str: the string to be splitted
  *
@@ -14,7 +64,7 @@
 char **strtow(char *str)
 {
 	char **str_array;
-	int words_count = 0, i = 0;
+	int words_count = 0;
 	char *s;
 
 	if (str == NULL || *str == '\0')
@@ -38,46 +88,17 @@ char **strtow(char *str)
 			s++;
 		}
 	}
+	if (words_count == 0)
+	{
+		return (NULL);
+	}
 	str_array = (char **)malloc((words_count + 1) * sizeof(char *));
 	if (str_array == NULL)
 	{
 		return (NULL);
 	}
 	s = str;
-	while (*s != '\0')
-	{
-		while (*s == ' ')
-		{
-			s++;
-		}
-		if (*s != '\0')
-		{
-			char *word_start = s;
-			int word_len;
-
-			while (*s != ' ' &&  *s != '\0')
-			{
-				s++;
-			}
-			word_len = s - word_start;
-			str_array[i] = (char *)malloc((word_len + 1) * sizeof(char));
-			if (str_array[i] == NULL)
-			{
-				int j;
-
-				for (j = 0; j < i; j++)
-				{
-					free(str_array[j]);
-				}
-				free(str_array);
-				return (NULL);
-			}
-			strncpy(str_array[i], word_start, word_len);
-			str_array[i][word_len] = '\0';
-			i++;
-
-		}
-	}
+	str_array = split_str(s, str_array);
 	str_array[words_count] = NULL;
 	return (str_array);
 }
