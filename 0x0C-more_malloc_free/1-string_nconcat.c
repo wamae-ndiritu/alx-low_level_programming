508997025065955a1fe2dd1ab8e47493bf60d231
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-13 21:00:29 +0000

    concatenating strings and returning them in a new space in memory

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ *
+ * @s1: the string to be appended
+ * @s2: the string that will be appeded to s1
+ * @n: number of characters of s2 to be concatenated to s1
+ *
+ * Description - concatenates two strings.
+ *
+ * Return: returns a pointer to a newly alocated
+ *	space in memory conataining s1, followed by n
+ *	bytes of s2, and null terminated.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *new_str, *str1, *str2;
+	int len1, len2, str_len;
+
+	if (s1 == NULL)
+	{
+		s1 = '\0';
+	}
+	else if (s2 == NULL)
+	{
+		s2 = '\0';
+	}
+	str1 = s1;
+	str2 = s2;
+	len1 = strlen(str1);
+	len2 = strlen(str2);
+	str_len = len1 + len2;
+	new_str = (char *)malloc((str_len + 1) * sizeof(char));
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(new_str, str1);
+	if ((int)n >= len2)
+	{
+		strncat(new_str, s2, len2);
+	}
+	strncat(new_str, s2, n);
+	return (new_str);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -3,3 +3,6 @@
 [0. Trust no one](./0-malloc_checked.c) - Creating a function that alocates memory using malloc. Protoype: `void *malloc_checked(unsigned int b);`. The function returns a pointer to the allocated memory. If `malloc` fails, the `malloc_checked` function should cause normal process termination with a status value of 98.
 
 
+[1. string_nconcat](./1-string_nconcat.c) - Creating a function that concatenates two strings. Prototype: `char *string_nconcat(char *s1, char *s2, unsigned int n);`. The returned pointer shall point to a newly allocated space in memory, which contains `s1`, followed by the first `n` bytes of `s2`, and null terminated. If the function fails, it should return NULL. If `n` is greater or equal to the length of `s2` then use the entire string `s2`. if NULL is passed, treat is as an empty string.
+
+
