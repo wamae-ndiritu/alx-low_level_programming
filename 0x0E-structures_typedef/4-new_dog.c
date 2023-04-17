48530613efa9a7d3693f3753f4ee5255d3fb0dc5
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-17 12:35:46 +0000

    updated function to create an instance of dog_t to copy the name and owner string

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
 /**
@@ -15,14 +16,29 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	char *new_name;
+	char *new_owner;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 	{
 		return (NULL);
 	}
-	dog->name = name;
+	/* allocating memory for copy of name and owner */
+	new_name = (char *)malloc((strlen(name) + 1) * sizeof(char));
+	new_owner = (char *)malloc((strlen(owner) + 1) * sizeof(char));
+	if (new_name == NULL || new_owner == NULL)
+	{
+		free(new_name);
+		free(new_owner);
+		free(dog);
+		return (NULL);
+	}
+	/* copy name & owner */
+	strcpy(new_name, name);
+	strcpy(new_owner, owner);
+	dog->name = new_name;
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = new_owner;
 	return (dog);
 }
