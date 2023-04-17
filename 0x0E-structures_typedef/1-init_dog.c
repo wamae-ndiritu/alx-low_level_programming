b3ce5ad7b2f4f4e56d7d544f2217c6771550a5cc
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-17 11:06:39 +0000

    created a functing to initialize the structure of a dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - Initialize the a dog
+ *
+ * @d: the structure of a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Return: Nothig.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -6,4 +6,7 @@
 - `age`, type = `float`
 - `owner`, type = `char *` 
 
+[1. A dog is the only thing on earth that loves you more than you love yourself](./1-init_dog.c) - Creating a function that initialize a variable of type `struct dog`.
+- Prototype: `void init_dog(struct dog *d, char *name, float age, char *owner);`
+
 
