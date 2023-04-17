7be7a1d95ca48e6e96efff1a2bfbc1f412c5ffae
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-17 12:54:52 +0000

    created a function to free previously allocated memory block for struct dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory block allocated for dogs
+ *
+ * @d: pointer to an instance of the dog
+ *
+ * Return: returns Nothing.
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -25,3 +25,7 @@
 - Return `NULL` if the function fails
 
 
+[5. How many legs does a dog have if you call his tail a leg? Four. Saying that a tail is a leg doesn't make it a leg](./5-free_dog) - Creating a function that frees dogs.
+- Prototype: `void free_dog(dog_t *d);`
+
+
