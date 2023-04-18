470e8332e95d2d68df32ee871a74d2ecba44b828
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-18 08:26:47 +0000

    modified print_dog function to print (nil) if NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,19 +17,11 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)\n");
-	}
-	else
-	{
-		printf("%s\n", d->name);
+		d->name = "(nil)";
 	}
 	if (d->owner == NULL)
 	{
-		printf("Owner: (nil)\n");
+		d->owner = "(nil)";
 	}
-	else
-	{
-		printf("%s\n", d->owner);
-	}
-	printf("Age: %f\n", d->age);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
