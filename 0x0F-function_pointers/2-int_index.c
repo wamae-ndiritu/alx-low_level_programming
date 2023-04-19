d8b72b30e593d37ec81bb012cdfa31941d7c87c1
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-19 17:04:36 +0000

    created a function that searches for an integer using a callback function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: an array of elements
+ * @size: the size of the array
+ * @cmp: pointer to the function to be used to compare values
+ *
+ * Return: returns the index of the first element,
+ *	-1 if no element matches or size less than or equal to 0.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
