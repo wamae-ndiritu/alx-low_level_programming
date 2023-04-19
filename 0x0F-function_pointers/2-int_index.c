dafb199b4dc3f660c98de3e9152535401de8c425
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-19 17:11:42 +0000

    updated the function int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,13 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
+	if (size <= 0)
 	{
 		return (0);
 	}
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
+		if (cmp(array[i]) != 0)
 			return (i);
 	}
 	return (-1);
