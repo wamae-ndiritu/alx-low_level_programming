0e9196f676ae8500dab08c62f2ed28e13c8098b9
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-19 16:20:48 +0000

    handled an errors in the inputs passed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,6 +14,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL || size == 0 || action == NULL)
+	{
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
