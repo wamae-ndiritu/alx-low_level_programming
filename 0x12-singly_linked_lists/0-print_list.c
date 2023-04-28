40887ee0bc20906b5a45a0ec4c3d2eac8d0e7174
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-04-28 05:11:55 +0000

    printing all elements of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: pointer to head node of the linked list
+ *
+ * Return: number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *current_node = h;
+	size_t count_nodes = 0;
+
+	while (current_node != NULL)
+	{
+		char len_buffer[16], *str;
+		size_t len = current_node->len;
+		size_t i = 0, j, str_len;
+
+		do {
+			len_buffer[i++] = (len % 10) + '0';
+			len /= 10;
+		} while (len != 0);
+		while (i-- > 0)
+		{
+			_putchar('[');
+			if (current_node->str == NULL)
+				_putchar(0 + '0');
+			else
+				_putchar(len_buffer[i]);
+			_putchar(']');
+		}
+		_putchar(' ');
+		str = current_node->str;
+		if (str == NULL)
+		{
+			str = "(nil)";
+			
+		}
+		str_len = strlen(str);
+		for (j = 0; j < str_len; j++)
+		{
+			_putchar(str[j]);
+		}
+		_putchar('\n');
+		count_nodes++;
+		current_node = current_node->next;
+	}
+	return (count_nodes);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stddef.h>
+int _putchar(char c);
+/**
+ * struc list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked ist node structure
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
+#endif
