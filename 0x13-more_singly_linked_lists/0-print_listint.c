e9435ca257ed8b846e9e50979a9013e1faf74699
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-05-03 16:51:34 +0000

    created a function to print all the elements of a linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to the first element of the list
+ *
+ * Return: returns the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current_list_node = h;
+	size_t node_count = 0;
+
+	if (current_list_node == NULL)
+		return (0);
+	while (current_list_node != NULL)
+	{
+		printf("%u\n", current_list_node->n);
+		node_count++;
+		current_list_node = current_list_node->next;
+	}
+	return (node_count);
+}
