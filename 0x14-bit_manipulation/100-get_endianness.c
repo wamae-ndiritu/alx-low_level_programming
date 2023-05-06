ab9faff5431fecb6858f54d93d6ad72ea3d545fe
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-05-06 10:13:43 +0000

    created a function to check the endianness of the computer

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	union
+	{
+		int i;
+		char ch[sizeof(int)];
+	} my_union;
+
+	my_union.i = 1;
+	if (my_union.ch[0] == 1)
+	{
+		/* machine is little-endian */
+		return (1);
+	}
+	else
+	{
+		/* machine is big-endian */
+		return (0);
+	}
+}
