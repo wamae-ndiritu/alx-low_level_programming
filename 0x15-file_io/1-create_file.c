f5a9a9884f00d7d5dcd9c6f1f5febaf35b51ed3e
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-05-09 07:36:05 +0000

    style: improve betty coding style
    This commit creates a space before the open parenthesis in the if statement

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 			fp = open(filename, O_WRONLY | O_TRUNC);
 			if (fp == -1)
 				return (-1);
-			if(truncate(filename, 0) == -1)
+			if (truncate(filename, 0) == -1)
 				return (-1);
 		}
 		return (-1);
