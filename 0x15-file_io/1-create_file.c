28fe79559329a129cfdf761bb1341868b11e1a3e
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-05-09 07:15:10 +0000

    feat: add function to create file
    This commits adds a new function to create a file that allows writing, or truncating if it already existed.
    The code for the function is in  in the main branch.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/**
+ * create_file - creates a file
+ * @filename: pointer to the file to be created
+ * @text_content: text content to be added to the created file
+ *
+ * Return: returns 1 on success, or -1 if filename is NULL
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+	ssize_t text_len, write_bytes;
+	mode_t mask;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	mask = umask(0);
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0600);
+	umask(mask);
+	if (fp == -1)
+	{
+		if (errno == EEXIST)
+		{
+			fp = open(filename, O_WRONLY | O_TRUNC);
+			if (fp == -1)
+				return (-1);
+		}
+		return (-1);
+	}
+	text_len = strlen(text_content);
+	write_bytes = write(fp, text_content, text_len);
+	if (write_bytes == -1 && write_bytes != text_len)
+	{
+		close(fp);
+		return (-1);
+	}
+	close(fp);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,5 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
