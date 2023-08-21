85be00307faecbb80aa4112e0ec939f45824e460
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-21 20:37:49 +0000

    Call C function in a dynamic library from python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+/**
+ * add - adds two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: returns the sum of the two integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - gets the difference of two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: returns the difference between the two integers
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * mul - gets the product of two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: returns the product of the two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * div - gets the division of two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: returns the division
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - gets the remainder after dividing two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: returns the remainder
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
