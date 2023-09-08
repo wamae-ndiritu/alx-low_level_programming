e48d73f8bdfdcf2cf866201d3f91e9d946ab1210
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-09-08 05:26:16 +0000

    Implement hash function using djb2 algorithm

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash the key using the djb2 algorithm
+ *
+ * @str: pointer to the key string to be hashed
+ *
+ * Return: returns the hashed value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash_val = 5381;
+	int ch;
+
+	while ((ch = *str++))
+		hash_val = ((hash_val << 5) + hash_val) ^ ch;
+
+	return (hash_val);
+}
